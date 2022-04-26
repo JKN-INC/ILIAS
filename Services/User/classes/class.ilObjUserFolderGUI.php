7c3e78910d9dfd0b39616bdad7c92815549e12fa
Evan <evantjackson@gmail.com>
2022-04-26 13:40:54 -0400

    Fixes using user improt parser if there are no roles set on the user xml import

--- Services/User/classes/class.ilObjUserFolderGUI.php
+++ Services/User/classes/class.ilObjUserFolderGUI.php
@@ -1097,6 +1097,11 @@ class ilObjUserFolderGUI extends ilObjectGUI
         // global roles
         $got_globals = false;
         $global_selects = array();
+        
+        $global_roles_assignment_info = $ui->input()->field()->text($this->lng->txt("roles_of_import_global"))
+            ->withDisabled(true)
+            ->withValue($this->lng->txt("assign_global_role"));
+
         foreach ($roles as $role_id => $role) {
             if ($role["type"] == "Global") {
                 if (!$got_globals) {
